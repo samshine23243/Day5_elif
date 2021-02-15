a60cd3b0aae76afc52219fca570645c1344c1c49
samshine <sameershinde23243@gmail.com>
2021-02-15 12:53:43 +0530

    example3 code added

--- elif.sh
+++ elif.sh
@@ -1,1 +1,22 @@
-echo "else if problems"
+echo "Enter number"
+read Num
+if [ $Num -eq "1" ]
+then
+   echo "Unit"
+elif [ $NUM -eq '10' ]
+then
+   echo "ten"
+elif [ $Num -eq "100" ]
+then
+   echo "Hundred"
+elif [ $Num -eq "1000" ]
+then
+   echo "Thousand"
+elif [ $Num -eq "10000" ]
+then
+   echo "Ten Thousand"
+elif [ $Num -eq "100000" ]
+then
+   echo "Lakh"
+fi
+
