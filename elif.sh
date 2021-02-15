9350013f463e283db53b219397f8a058e48177a6
samshine <sameershinde23243@gmail.com>
2021-02-15 12:49:00 +0530

    example 1 code added

--- elif.sh
+++ elif.sh
@@ -1,1 +1,33 @@
-echo "else if problems"
+randomNum=$((1 + $RANDOM % 10))
+if [ $randomNum -eq "1" ]
+then
+   echo "One"
+elif [ $randomNum -eq "2" ]
+then
+   echo "two"
+elif [ $randomNum -eq "3" ]
+then
+   echo "three"
+elif [ $randomNum -eq "4" ]
+then
+   echo "four"
+elif [ $randomNum -eq "5" ]
+then
+   echo "five"
+elif [ $randomNum -eq "6" ]
+then
+   echo "six"
+elif [ $randomNum -eq "7" ]
+then
+   echo "seven"
+elif [ $randomNum -eq "8" ]
+then
+   echo "eight"
+elif [ $randomNum -eq "9" ]
+then
+   echo "nine"
+elif [ $randomNum -eq "10" ]
+then
+   echo "ten"
+fi
+
