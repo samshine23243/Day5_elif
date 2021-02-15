9a57f7b7cff6ba50f296c4a06dc734bf062b0954
samshine <sameershinde23243@gmail.com>
2021-02-15 12:50:57 +0530

    example3 code added

--- elif.sh
+++ elif.sh
@@ -1,1 +1,23 @@
-echo "else if problems"
+randomNum=$((1 + $RANDOM % 7))
+if [ $randomNum -eq "1" ]
+then
+   echo "Sunday"
+elif [ $randomNum -eq "2" ]
+then
+   echo "Monday"
+elif [ $randomNum -eq "3" ]
+then
+   echo "Tuesday"
+elif [ $randomNum -eq "4" ]
+then
+   echo "Wednesday"
+elif [ $randomNum -eq "5" ]
+then
+   echo "Thursday"
+elif [ $randomNum -eq "6" ]
+then
+   echo "Firday"
+else
+   echo "Saturday"
+fi
+
